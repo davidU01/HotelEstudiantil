e81579a799af864784e5525620348bfa019015c7
David Abril <David.Abril@uptc.edu.co>
2022-03-30 16:17:24 -0500

    Configuraciones de propiedad

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,10 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="hostel";
+$dbuser="u5kqcja6qbfwixja";
+$dbpass="vzKTAoGQctvrmZ3BuEoG";
+$host="bhxcvvnq9j6lo7aynk87-mysql.services.clever-cloud.com";
+$db="bhxcvvnq9j6lo7aynk87";
+
+
+
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
 ?>
--- admin/includes/dbcontroller.php
+++ admin/includes/dbcontroller.php
@@ -1,8 +1,8 @@
 <?php
-$DB_host = "localhost";
-$DB_user = "root";
-$DB_pass = "";
-$DB_name = "hostel";
+$DB_host = "bhxcvvnq9j6lo7aynk87-mysql.services.clever-cloud.com";
+$DB_user = "u5kqcja6qbfwixja";
+$DB_pass = "vzKTAoGQctvrmZ3BuEoG";
+$DB_name = "bhxcvvnq9j6lo7aynk87";
 try
 {
  $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
