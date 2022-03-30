878c583623f9b339031c37d31dd78d1c21041b05
David Abril <David.Abril@uptc.edu.co>
2022-03-30 00:36:08 -0500

    agregadas configuraciones

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,11 @@
 <?php
 $dbuser="root";
 $dbpass="";
-$host="localhost";
+$host: 'bhxcvvnq9j6lo7aynk87-mysql.services.clever-cloud.com';
 $db="hostel";
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+$port: '3306';
+$user:'u5kqcja6qbfwixja';
+$password:'vzKTAoGQctvrmZ3BuEoG';
+$database:'bhxcvvnq9j6lo7aynk87';
 ?>
